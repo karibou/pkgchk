86b10f5342a0ed1ad70c93f5c568f83257287c84
Louis Bouchard <louis.bouchard@canonical.com>
2014-09-11 12:54:03 +0200

    Initial version
    Signed-off-by: Louis Bouchard <louis.bouchard@canonical.com>

---
+++ check-policy.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+import apt
+
+def init_cache(rootdir):
+    cache = apt.Cache(rootdir=rootdir)
+    cache.update()
+    pkg="apt"
+    if pkg in cache:
+        print("Found pkg %s" % pkg)
+
+def is_available(dist):
+    etcdir = os.path.join(dist, "etc", "apt")
+    if not os.path.exists(etcdir):
+        os.makedirs(etcdir)
+    with open(os.path.join(etcdir, "sources.list"), "w") as f:
+        for pocket in ["", "-updates", "-security", "-proposed"]:
+            f.write("deb http://archive.ubuntu.com/ubuntu %s%s main restricted universe multiverse\n" % (dist, pocket))
+    cache = apt.Cache(rootdir="./%s" % dist)
+    cache.update()
+    for line in sys.stdin.readlines():
+        if not line:
+            continue
+        want_pkg, want_ver = line.split()
+        if not want_pkg in cache:
+            print("%s missing")
+            return False
+        if want_ver in cache[want_pkg].versions:
+            break
+        else:
+            print("%s not available for %s" % (want_ver, want_pkg))
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    init_cache("./sosreport-TrustyS-testdump-20140829163304/etc/apt")
